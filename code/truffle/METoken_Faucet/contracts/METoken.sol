71c81664e415127c02cae17e7de842b5509b2707
inlout <code@inlout.com>
2018-06-26 11:37:13 +0800

    update

---
+++ code/truffle/METoken_Faucet/contracts/METoken.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.18;
+
+import 'zeppelin-solidity/contracts/token/ERC20/StandardToken.sol';
+
+contract METoken is StandardToken {
+    string public constant name = 'Mastering Ethereum Token';
+    string public constant symbol = 'MET';
+    uint8 public constant decimals = 2;
+    uint constant _initial_supply = 2100000000;
+
+    function METoken() public {
+        totalSupply_ = _initial_supply;
+        balances[msg.sender] = _initial_supply;
+        Transfer(address(0), msg.sender, _initial_supply);
+    }
+}
